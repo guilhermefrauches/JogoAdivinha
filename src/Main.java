de6215f688846497fdc4b2e6cd34049812109f79
Guilherme Moreira <90403785+guilhermefrauches@users.noreply.github.com>
2025-06-02 15:08:03 -0300

    add cor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
+
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         Random random = new Random();
         int numeroSecreto = random.nextInt(101);
@@ -10,19 +16,19 @@ public class Main {
         boolean acertou = false;
 
         while (!acertou) {
-            System.out.println("Digite um palpite: ");
+            System.out.println( PURPLE + "Digite um número: " + RESET);
             palpite = sc.nextInt();
 
             if (palpite == numeroSecreto) {
-                System.out.println("Parabéns! Você acertou!");
+                System.out.println(GREEN + "Parabéns! Você acertou!" + RESET);
                 acertou = true;
             } else if (palpite > 100 || palpite < 0) {
-                System.out.println("O numero é inválido, tente outro número.");
+                System.out.println(RED+ "O numero é inválido, tente outro número." + RESET );
 
             } else if (palpite < numeroSecreto) {
-                System.out.println("O número é maior!");
+                System.out.println(BLUE + "O número é maior!" + RESET);
             } else {
-                System.out.println("O número é menor!");
+                System.out.println(BLUE + "O número é menor!" + RESET);
             }
         }
         sc.close();
