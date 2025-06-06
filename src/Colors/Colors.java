b27d89656eb50f7788786e363bc29a0764d0aafd
Guilherme Moreira <90403785+guilhermefrauches@users.noreply.github.com>
2025-06-06 17:22:24 -0300

    Modularização de código

---
+++ src/Colors/Colors.java
@@ -0,0 +1,9 @@
+package Colors;
+
+public class Colors {
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
+import static Colors.Colors.*;
 public class Main {
-
-    public static final String GREEN = "\u001B[32m";
-    public static final String RED = "\u001B[31m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         Random random = new Random();
@@ -16,7 +11,7 @@ public class Main {
         boolean acertou = false;
 
         while (!acertou) {
-            System.out.println( PURPLE + "Digite um número: " + RESET);
+            System.out.println(PURPLE + "Digite um número: " + RESET);
             palpite = sc.nextInt();
 
             if (palpite == numeroSecreto) {
