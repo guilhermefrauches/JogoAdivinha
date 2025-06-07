509b6ad65d9d16157026f80cd3728cadf48e25a1
Guilherme Moreira <90403785+guilhermefrauches@users.noreply.github.com>
2025-06-07 20:38:49 -0300

    add  tratamento de exceção

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 import static Colors.Colors.*;
@@ -11,21 +12,29 @@ public class Main {
         boolean acertou = false;
 
         while (!acertou) {
-            System.out.println(PURPLE + "Digite um número: " + RESET);
-            palpite = sc.nextInt();
+            System.out.println(PURPLE + "Digite um número de 0 á 100: " + RESET);
 
-            if (palpite == numeroSecreto) {
-                System.out.println(GREEN + "Parabéns! Você acertou!" + RESET);
-                acertou = true;
-            } else if (palpite > 100 || palpite < 0) {
-                System.out.println(RED+ "O numero é inválido, tente outro número." + RESET );
+            try {
+                palpite = sc.nextInt();
 
-            } else if (palpite < numeroSecreto) {
-                System.out.println(BLUE + "O número é maior!" + RESET);
-            } else {
-                System.out.println(BLUE + "O número é menor!" + RESET);
+                if (palpite == numeroSecreto) {
+                    System.out.println(GREEN + "Parabéns! Você acertou!" + RESET);
+                    acertou = true;
+                } else if (palpite > 100 || palpite < 0) {
+                    System.out.println(RED + "O número é inválido, tente outro número." + RESET);
+                } else if (palpite < numeroSecreto) {
+                    System.out.println(BLUE + "O número é maior!" + RESET);
+                } else {
+                    System.out.println(BLUE + "O número é menor!" + RESET);
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println(RED + "Entrada inválida. Digite um número inteiro." + RESET);
+                sc.next();
+                continue;
             }
         }
+
         sc.close();
     }
 }
